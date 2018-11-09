ae2bc0745a08fa2aec810ef62823621f9e312378
Anggi Wijaya <crystalguy404@gmail.com>
2018-11-09 11:30:23 +0700

    Loop when not true

--- ifexample.py
+++ ifexample.py
@@ -1,14 +1,17 @@
+listuname = ['anggi','admin']
 print('Hallow, Masukkan Username .!')
 username = input()
 print('Hai,'+ username)
-if username != 'anggi' or username !='admin':
+if username.strip() not in listuname:
     print('maaf ' + username + ' belum terdaftar,silahkan daftar terlebih dahulu')
     exit()
 else: 
-    print('masukkan password')
-password = input()
-if password != 'snake':
+    print('Masukkan Password.!')
+    password = input()
+while password != 'snake':
     print('Password Salah .!')
-else:
-    print('Hai' + username + ', Selamat Datang Kembali')
+    print('Masukkan Password.!')
+    password = input()
+    
+print('Hai ' + username + ', Selamat Datang Kembali')
 
