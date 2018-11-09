d191d9a2e33bfd4cc0afa5cd006e091d15beb9e7
Anggi Wijaya <crystalguy404@gmail.com>
2018-11-09 11:30:23 +0700

    Contoh If

--- ifexample.py
+++ ifexample.py
@@ -1,7 +1,8 @@
+listuname = ['anggi','admin']
 print('Hallow, Masukkan Username .!')
 username = input()
 print('Hai,'+ username)
-if username != 'anggi' or username !='admin':
+if username.strip() not in listuname:
     print('maaf ' + username + ' belum terdaftar,silahkan daftar terlebih dahulu')
     exit()
 else: 
@@ -10,5 +11,4 @@ password = input()
 if password != 'snake':
     print('Password Salah .!')
 else:
-    print('Hai' + username + ', Selamat Datang Kembali')
-
+    print('Hai ' + username + ', Selamat Datang Kembali')
